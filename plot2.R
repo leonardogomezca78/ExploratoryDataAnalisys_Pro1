dfaff5561e4ffa65e1330ba197951ce0ea9aca85
leonardogomezca78 <61802244+leonardogomezca78@users.noreply.github.com>
2020-04-13 15:09:37 -0500

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,16 @@
+
+if(!file.exists('data')) dir.create('data')
+leo78 <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+if(!file.exists('./data/household_power_consumption.zip')) download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+if(!file.exists('./data/household_power_consumption.txt')) unzip('./data/household_power_consumption.zip', exdir = './data')
+file <- file('./data/household_power_consumption.txt')
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(file),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y") 
+DateTime <- paste(as.Date(data$Date), data$Time) 
+data$DateTime <- as.POSIXct(DateTime)
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+png(filename = 'plot2.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+dev.off()
